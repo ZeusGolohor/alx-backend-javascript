7790f9a3561c17e8608f0a1be5e6f65b84c38af9
zeusholohor <zeusgolohor@gmail.com>
2024-04-26 01:13:47 +0000

    Implement a class named Currency:
    - Constructor attributes:
    code (String)
    name (String)
    Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
    Implement a getter and setter for each attribute.
    Implement a method named displayFullCurrency that will return the attributes in the following format name (code).

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
