2443ecb4d6ba3918991c803e057bfe099c90fbc7
zeusholohor <zeusgolohor@gmail.com>
2024-04-27 04:24:00 +0000

    Import Car from 10-car.js.
    Implement a class named EVCar that extends the Car class:
    
    Constructor attributes:
    brand (String)
    motor (String)
    color (String)
    range (String)
    Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
    For privacy reasons, when cloneCar is called on a EVCar object, the object returned should be an instance of Car instead of EVCar.
    bob@dylan:~$ cat 100-main.js
    import EVCar from './100-evcar.js';
    
    const ec1 = new EVCar(Tesla, Turbo, Red, 250);
    console.log(ec1);
    
    const ec2 = ec1.cloneCar();
    console.log(ec2);
    
    bob@dylan:~$
    bob@dylan:~$ npm run dev 100-main.js
    EVCar {
      _brand: 'Tesla',
      _motor: 'Turbo',
      _color: 'Red',
      _range: '250'
    }
    Car { _brand: undefined, _motor: undefined, _color: undefined }
    bob@dylan:~$

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,48 @@
+import Car from "./10-car.js";
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super();
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(newBrand) {
+    this._brand = newBrand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(newMotor) {
+    this._motor = newMotor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor) {
+    this._color = newColor;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(newRange) {
+    this._range = newRange;
+  }
+
+  cloneCar() {
+    return  new Car(this._brand, this._motor, this._color);
+  }
+}
+
