21c7c16fe9cf27dd77be0bc8b6a8254cd2c0e190
zeusgolohor <zeusgolohor@gmail.com>
2024-06-19 01:39:25 +0000

    In a file named 7-http_express.js, recreate the small HTTP server using Express:
    It should be assigned to the variable app and this one must be exported
    HTTP server should listen on port 1245
    It should return plain text
    When the URL path is /, it should display Hello Holberton School! in the page body
    When the URL path is /students, it should display This is the list of our students followed by the same content as the file 3-read_file_async.js (with and without the database) - the name of the database must be passed as argument of the file
    CSV file can contain empty lines (at the end) - and they are not a valid student!
    Terminal 1:

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+const PORT = 1245;
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+
+async function countStudents(dataPath) {
+  if (!dataPath) {
+    throw new Error('Cannot load the database');
+  }
+
+  try {
+    const data = await fs.readFile(dataPath, 'utf-8');
+    const lines = data.trim().split('\n');
+    const studentGroups = {};
+
+    const header = lines[0].split(',');
+    const studentFields = header.slice(0, header.length - 1);
+
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i];
+      const fields = line.split(',');
+      const field = fields[fields.length - 1];
+      
+      if (!studentGroups[field]) {
+        studentGroups[field] = [];
+      }
+
+      const student = {};
+      for (let j = 0; j < studentFields.length; j++) {
+        student[studentFields[j].trim()] = fields[j].trim();
+      }
+      studentGroups[field].push(student);
+    }
+
+    const reportParts = [];
+    const totalStudents = lines.length - 1;
+    reportParts.push(`Number of students: ${totalStudents}`);
+    
+    for (const [field, students] of Object.entries(studentGroups)) {
+      const studentList = students.map(student => student.firstname).join(', ');
+      reportParts.push(`Number of students in ${field}: ${students.length}. List: ${studentList}`);
+    }
+
+    return reportParts.join('\n');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (_, res) => {
+  try {
+    const report = await countStudents(DB_FILE);
+    const responseParts = [`This is the list of our students\n${report}`];
+    
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', Buffer.byteLength(responseParts.join('\n')));
+    res.status(200).send(responseParts.join('\n'));
+  } catch (err) {
+    const errorResponse = `This is the list of our students\n${err.message}`;
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', Buffer.byteLength(errorResponse));
+    res.status(500).send(errorResponse);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
