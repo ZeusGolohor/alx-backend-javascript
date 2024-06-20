2a9acec95e3baf2180dc9c5bd6dc1f9278bda946
zeusgolohor <zeusgolohor@gmail.com>
2024-06-20 23:49:26 +0000

    Install Mocha using npm:
    Set up a scripts in your package.json to quickly run Mocha using npm test
    You have to use assert
    Create a new file named 0-calcul.js:
    
    Create a function named calculateNumber. It should accepts two arguments (number) a and b
    The function should round a and b and return the sum of it
    Test cases
    
    Create a file 0-calcul.test.js that contains test cases of this function
    You can assume a and b are always number
    Tests should be around the “rounded” part
    Tips:
    
    For the sake of the example, this test suite is slightly extreme and probably not needed
    However, remember that your tests should not only verify what a function is supposed to do, but also the edge cases
    Requirements:
    
    You have to use assert
    You should be able to run the test suite using npm test 0-calcul.test.js
    Every test should pass without any warning
    Expected output
    
    > const calculateNumber = require("./0-calcul.js");
    > calculateNumber(1, 3)
    4
    > calculateNumber(1, 3.7)
    5
    > calculateNumber(1.2, 3.7)
    5
    > calculateNumber(1.5, 3.7)
    6
    >
    Run test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,5 +7,10 @@ describe('calculateNumber', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
     assert.equal(calculateNumber(1.2, 3.7), 5);
     assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(2.3, 4.7), 7);
+    assert.equal(calculateNumber(0.1, 0.1), 0);
+    assert.equal(calculateNumber(1.9, 1.9), 4);
+    assert.equal(calculateNumber(1.1, 2.2), 3);
+    assert.equal(calculateNumber(3.6, 1.2), 5);
   });
 });
