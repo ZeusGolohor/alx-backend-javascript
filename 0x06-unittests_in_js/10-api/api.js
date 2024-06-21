7bdc95c110fd4e8230317accd38845275eaf8828
zeusgolohor <zeusgolohor@gmail.com>
2024-06-21 04:12:53 +0000

    In a folder 10-api, reusing the previous project in 9-api (package.json, api.js and api.test.js)
    Modify the file api.js:
    
    Add an endpoint GET /available_payments that returns an object with the following structure:
    {
      payment_methods: {
        credit_cards: true,
        paypal: false
      }
    }
    Add an endpoint POST /login that returns the message Welcome :username where :username is the value of the body variable userName.
    Modify the file api.test.js:
    
    Add a test suite for the /login endpoint
    Add a test suite for the /available_payments endpoint

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
+});
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id_num = req.params.id;
+  res.send(`Payment methods for cart ${id_num}`);
+});
+app.get('/available_payments', (_req, res) => {
+  res.json({payment_methods: {credit_cards: true, paypal: false}});
+});
+app.post('/login', (req, res) => {
+  let user = '';
+  if (req.body) {
+    user = req.body.userName;
+  }
+  res.send(`Welcome ${username}`);
+});
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+module.exports = app;
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,35 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API integration test', () => {
+  const url = 'http://localhost:7865';
+  it('GET url', (done) => {
+    request.get(`${url}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200)
+	  expect(body).to.be.equal('Welcome to the payment system');
+	  done();
+    });
+  });
+  it('GET /cart/:id', (done) => {
+    request.get(`${url}/cart/360`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+	  expect(body).to.be.equal('Payment methods for cart 360');
+	  done();
+    });
+  });
+  it('GET /available_payments', (done) => {
+    request.get(`${urlavailable_payments`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+	  expect(JSON.parse(body))
+	    .to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
+	  done();
+    });
+  });
+  it('POST /login', (done) => {
+    request.post(`${url}/login`, {json: {userName: 'Zeus'}}, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+	  expect(body).to.be.equal('Welcome Zeus');
+	  done();
+    });
+  });
+});
